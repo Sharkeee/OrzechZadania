051b1f268ea4a17d94c160639e23da374766c929
Sharkeee <sharkmen1996@gmail.com>
2017-12-18 23:36:41 +0100

    Zadanie4 0.2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import javax.swing.JOptionPane;
+
 public class Main {
 
     public static void main(String[] args) {
 
         int[] tablica = new int[9];
 
-        for(int i=0; i<=tablica.length;i++) {
-             int x = Integer.parseInt(JOptionPane.showInputDialog("Wprowadz wartosc elementu ["+(i)+"]"));
-             tablica[i] = x;
-            System.out.println("Wartosc elementu [" + (i) + "]:" + x);
+        for (int i = 0; i <= tablica.length; i++) {
+            int x = Integer.parseInt(JOptionPane.showInputDialog("Wprowadz wartosc elementu [" + i + "]"));
+            tablica[i] = x;
+            System.out.println("Wartosc elementu [" + i + "]:" + x);
         }
 
     }
