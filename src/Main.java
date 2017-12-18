5d23e21fa6ac59f6192a1f4fc0fab06d27a1eef3
Sharkeee <sharkmen1996@gmail.com>
2017-12-18 23:34:59 +0100

    Zadanie 4 gotowe

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
+import javax.swing.JOptionPane;
 public class Main {
 
     public static void main(String[] args) {
 
+        int[] tablica = new int[9];
 
-        Punkt punkt = new Punkt();
-        punkt.ustawWspolrzedne();
-        punkt.pobierzPunkt();
+        for(int i=0; i<=tablica.length;i++) {
+             int x = Integer.parseInt(JOptionPane.showInputDialog("Wprowadz wartosc elementu ["+(i)+"]"));
+             tablica[i] = x;
+            System.out.println("Wartosc elementu [" + (i) + "]:" + x);
+        }
+
     }
 }
